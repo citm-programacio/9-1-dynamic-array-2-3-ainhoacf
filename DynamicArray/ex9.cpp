63ebafef0fa40543430c116d7e3c5976dd9adf8c
ainhoacf <ainhoa.carayol@estudiantat.upc.edu>
2024-11-13 13:12:09 +0100

    a

--- DynamicArray/ex9.cpp
+++ DynamicArray/ex9.cpp
@@ -1,13 +1,42 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-class a {
-
-	a(): (),(){}
-};
-
-
-
 int main() {
+    int* array = nullptr; //Array vacio
+    int tamany = 0;
+    int num;
 
+    while (1) {
+        cout << "Entra un numero: ";
+        cin >> num; //Analizo el numero que escribe el usuario
+
+        int* newArray = new int[tamany + 1]; //Creo un nuevo array con un espacio más
+
+        int i = 0;
+       
+        while (i < tamany && array[i] < num) {//
+            newArray[i] = array[i];
+            i++;
+        }
+        newArray[i] = num;
+
+        for (int j = i; j < tamany; j++) {
+            newArray[j + 1] = array[j];
+        }
+        delete[] array; //Elimino el antiguo
+
+        array = newArray;
+        tamany++;
+
+        cout << "[";
+        for (int k = 0; k < tamany; k++) {
+            cout << array[k];
+            if (k < tamany - 1) cout << ", ";
+        }
+        cout << "]" << endl;
+    }
+    delete[] array;
+    return 0;
 }
+
